2d0e246e11d72bd8208935df522519eed26b4244
kat test <e_aksenova80@mail.ru>
2023-03-23 14:05:23 +0300

    Main.java change

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int wallet = 250; // остаток на счете
+        int amount = 200; // сумма пополнения
+        int bonus; // бонус за пополнение свыше 1000 р.
+        int total; // итоговая сумма счета
+
+        if (amount>1000)
+        {
+            bonus = (wallet + amount) / 100;
+        } else {
+            bonus = 0;
+        }
+        total = wallet + amount + bonus;
+
+
+        System.out.println("Сумма итогового счета:" + total);
     }
 }
